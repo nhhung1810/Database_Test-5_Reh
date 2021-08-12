a8bc9185525f4657ca2f52663a7ca912df5ffb28
Bacon <nguyenlamtuong2001@gmail.com>
2021-08-12 14:27:50 +0700

    done music

---
+++ Rehearsal_CS486_Team11/Rehearsal_CS486_Team11/MusicList.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Rehearsal_CS486_Team11
+{
+    partial class MusicList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowPanel
+            // 
+            this.flowPanel.Location = new System.Drawing.Point(13, 13);
+            this.flowPanel.Name = "flowPanel";
+            this.flowPanel.Size = new System.Drawing.Size(775, 425);
+            this.flowPanel.TabIndex = 0;
+            this.flowPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.flowPanel_Paint);
+            // 
+            // MusicList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowPanel);
+            this.Name = "MusicList";
+            this.Text = "MusicList";
+            this.Load += new System.EventHandler(this.MusicList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowPanel;
+    }
+}
--- Rehearsal_CS486_Team11/Rehearsal_CS486_Team11/Program.cs
+++ Rehearsal_CS486_Team11/Rehearsal_CS486_Team11/Program.cs
@@ -16,7 +16,7 @@ namespace Rehearsal_CS486_Team11
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MusicList());
         }
     }
 }
