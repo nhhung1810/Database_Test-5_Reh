9c87d882238fe7b7a35c19a0d78068cfa8ff6164
DuongVanKhoa19125099 <63323284+DuongVanKhoa19125099@users.noreply.github.com>
2021-08-12 14:26:03 +0700

    Tmp

---
+++ Rehearsal_CS486_Team11/Rehearsal_CS486_Team11/FormAboutUsTeam11.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Rehearsal_CS486_Team11
+{
+    partial class FormAboutUsTeam11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nguyen Huu Hung - 19125000";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(555, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Duty: ....";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Duty: ....";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nguyen Huu Hung - 19125000";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(204, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Duty: ....";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(164, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(225, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nguyen Huu Hung - 19125000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(204, 332);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Duty: ....";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(164, 301);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(225, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Nguyen Huu Hung - 19125000";
+            // 
+            // FormAboutUsTeam11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAboutUsTeam11";
+            this.Text = "FormAboutUsTeam11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ Rehearsal_CS486_Team11/Rehearsal_CS486_Team11/FormKhoa.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Rehearsal_CS486_Team11
+{
+    public partial class FormKhoa : Form
+    {
+        public FormKhoa()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void FormKhoa_Load(object sender, EventArgs e)
+        {
+            flpMusicList.AutoScroll = true;
+            flpMusicList.FlowDirection = FlowDirection.TopDown;
+            flpMusicList.WrapContents = false;
+            initMusicList();
+        }
+        public void initMusicList()
+        {
+            try
+            {
+                SqlConnection cnn = new SqlConnection(@"Data Source=LAPTOP-SMBGUUTC\SQLEXPRESS01;Initial Catalog=CS468_team11_DB;Integrated Security=True");
+                cnn.Open();
+                string sql = "select * from Category where id = Parentid";
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, cnn);
+                DataSet data = new DataSet();
+                adapter.Fill(data);
+                if (data.Tables.Count > 0)
+                {
+                    foreach(DataRow row in data.Tables[0].Rows)
+                    {
+                        int[] catID;
+                        string[] childCatName = null;
+                        DataSet dataTmp = new DataSet();
+                        sql = "select * from Category where id <> Parenid and Parentid = "
+                            + row[0].ToString();
+                        adapter = new SqlDataAdapter(sql, cnn);
+                        if(dataTmp.Tables.Count > 0)
+                        {
+                            int countUp = 0;
+                            catID = new int[dataTmp.Tables[0].Rows.Count];
+                            childCatName = new string[dataTmp.Tables[0].Rows.Count];
+                            foreach(DataRow rowTmp in dataTmp.Tables[0].Rows)
+                            {
+                                catID[countUp] = int.Parse(rowTmp[0].ToString());
+                                childCatName[countUp] = rowTmp[1].ToString();
+                            }
+                            addSong(dataTmp.Tables[0].Rows.Count, row[1].ToString(), childCatName, catID);
+                        } 
+                    }
+                }
+
+                cnn.Close();
+            }
+            catch (Exception s)
+            {
+                MessageBox.Show(s.ToString());
+            }
+        }
+        private Label _mainCategory;
+        private Button[] _childCategory = null;
+        public void addSong(int nbCategory, string mainCategory, string[] childCategory, int[] catID)
+        {
+            if (nbCategory < 1 || nbCategory != childCategory.Length)
+                return;
+            int width = 0;
+            _childCategory = new Button[nbCategory];
+            _mainCategory = new Label { Text = mainCategory };
+            _mainCategory.Location = new Point(0, 0);
+            _mainCategory.Height = 50;
+            _mainCategory.FlatStyle = FlatStyle.Flat;
+            _mainCategory.ForeColor = Color.Blue;
+            _mainCategory.Font = new Font("Georgia", 12);
+            width += _mainCategory.Width + 20;
+
+            for (int i = 0; i < nbCategory; i++)
+            {
+                _childCategory[i] = new Button();
+                _childCategory[i].Text = childCategory[i];
+                _childCategory[i].Location = new Point(width, 0);
+                _childCategory[i].Name = catID[i].ToString();
+                _childCategory[i].Click += new EventHandler(button_click);
+                width += _childCategory[i].Width + 20;
+            }
+
+            FlowLayoutPanel flpTmp = new FlowLayoutPanel();
+            flpTmp.Height = _mainCategory.Height;
+            flpTmp.Width = width + 20;
+            flpTmp.Controls.Add(_mainCategory);
+            for (int i = 0; i < nbCategory; i++)
+                flpTmp.Controls.Add(_childCategory[i]);
+
+            flpMusicList.Controls.Add(flpTmp);
+        }
+        public void button_click(object sender, EventArgs s)
+        {
+            Button button = sender as Button;
+            clrHotSongPanel();
+            //initHotSongPanel(int.Parse(button.Name));
+        }
+        public void clrHotSongPanel()
+        {
+            //hotSongPanel.Controls.Clear();
+        }
+    }
+}
