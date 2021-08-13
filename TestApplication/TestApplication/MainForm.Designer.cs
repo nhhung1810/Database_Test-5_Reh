01d5630e615ab50d0407c03ea72aee9ee86b170c
nhhung1810 <59091317+yauangon@users.noreply.github.com>
2021-08-13 21:03:15 +0700

    add button

--- TestApplication/TestApplication/MainForm.Designer.cs
+++ TestApplication/TestApplication/MainForm.Designer.cs
@@ -33,6 +33,12 @@ namespace TestApplication
             this.label2 = new System.Windows.Forms.Label();
             this.viewButton = new System.Windows.Forms.Button();
             this.editButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,7 +48,7 @@ namespace TestApplication
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(262, 61);
+            this.label1.Location = new System.Drawing.Point(254, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 25);
             this.label1.TabIndex = 0;
@@ -55,7 +61,7 @@ namespace TestApplication
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(342, 61);
+            this.label2.Location = new System.Drawing.Point(334, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 25);
             this.label2.TabIndex = 1;
@@ -66,7 +72,7 @@ namespace TestApplication
             this.viewButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.viewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.viewButton.Location = new System.Drawing.Point(207, 97);
+            this.viewButton.Location = new System.Drawing.Point(199, 80);
             this.viewButton.Name = "viewButton";
             this.viewButton.Size = new System.Drawing.Size(110, 74);
             this.viewButton.TabIndex = 2;
@@ -79,7 +85,7 @@ namespace TestApplication
             this.editButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.editButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.editButton.Location = new System.Drawing.Point(347, 97);
+            this.editButton.Location = new System.Drawing.Point(338, 80);
             this.editButton.Name = "editButton";
             this.editButton.Size = new System.Drawing.Size(110, 74);
             this.editButton.TabIndex = 3;
@@ -87,11 +93,73 @@ namespace TestApplication
             this.editButton.UseVisualStyleBackColor = true;
             this.editButton.Click += new System.EventHandler(this.editButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 60);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 55);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(339, 352);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 60);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(199, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 60);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(694, 450);
+            this.ClientSize = new System.Drawing.Size(693, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.editButton);
             this.Controls.Add(this.viewButton);
             this.Controls.Add(this.label2);
@@ -109,6 +177,12 @@ namespace TestApplication
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button viewButton;
         private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- TestApplication/TestApplication/MainForm.cs
+++ TestApplication/TestApplication/MainForm.cs
@@ -38,5 +38,15 @@ namespace TestApplication
             Set.EditForm ef = new Set.EditForm();
             ef.Show();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            return;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            return;
+        }
     }
 }
